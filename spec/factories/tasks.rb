ed0a5ae33c4ce055a2f81d97f9d2a54c76d243b7
lompo <lompo@iuj.ac.jp>
2020-11-13 17:05:58 +0900

    rebuilt ER diagram,fixed spec factories and rails v

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,8 +1,16 @@
 FactoryBot.define do
   factory :task do
-    factory :task do
     title { 'title test' }
     content { 'content test' }
+    deadline { '2020-09-03 17:00:00' }
+    status { 'status' }
+    priority { '0' }
   end
-end
+  factory :second_task, class: Task do
+    title { 'title test 2' }
+    description { 'content test 2' }
+    deadline { '2020-09-11 00:00:00' }
+    status { 'pending' }
+    priority { '0' }
+  end
 end
