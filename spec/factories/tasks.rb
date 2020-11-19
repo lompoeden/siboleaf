8bb90fa60db81f3430153b511b2629b20896b392
lompo <lompo@iuj.ac.jp>
2020-11-19 15:24:58 +0900

    set time zone, and spec test

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,11 +1,14 @@
 FactoryBot.define do
   factory :task do
-    title { 'Task Name' }
-    content { 'Task Details' }
+    title { 'title test' }
+    content { 'content test 1' }
   end
   factory :second_task, class: Task do
     title { 'title test 2' }
-    description { 'content' }
-
+    content { 'content test 2' }
+  end
+  factory :third_task, class: Task do
+    title { 'title test 3' }
+    content { 'content test 3' }
   end
 end
