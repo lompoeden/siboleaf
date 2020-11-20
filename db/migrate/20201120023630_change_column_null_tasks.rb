a879e6e0c9a3c9a3f1a70edf500b14b1b9b1243d
lompo <lompo@iuj.ac.jp>
2020-11-20 16:17:42 +0900

    db constraint,i18n mechanism and sorting

--- db/migrate/20201120023630_change_column_null_tasks.rb
+++ db/migrate/20201120023630_change_column_null_tasks.rb
@@ -1,7 +1,5 @@
 class ChangeColumnNullTasks < ActiveRecord::Migration[5.2]
   def change
-    hange_column :tasks, :start_date, :datetime, null: false
-        change_column :tasks, :end_date, :datetime, null: false
-        change_column :tasks, :status, :string, null: false
-      end
-    end
+    change_column:tasks, :title, :string, null: false
+end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_10_140745) do
+ActiveRecord::Schema.define(version: 2020_11_20_023630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2020_11_10_140745) do
   create_table "tasks", force: :cascade do |t|
     t.string "title", null: false
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.datetime "startdate"
     t.integer "priority", default: 0
     t.string "status", default: "pending"
