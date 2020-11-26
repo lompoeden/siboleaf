134e004929e4a968f7481f7ddbfac0511c250abe
lompo <lompo@iuj.ac.jp>
2020-11-26 11:31:43 +0900

    spec test

--- spec/system/task.spec.rb
+++ spec/system/task.spec.rb
@@ -1,55 +1,143 @@
 require 'rails_helper'
 RSpec.describe "Tasks management function", type: :system do
 
-  describe 'new feautures' do
-    context 'case was the task to create a new task' do
-      it 'should display the new task created' do
-        visit new_task_path
-        fill_in "Task Name", with: 'title test'
-        fill_in "Task Details", with: 'content test'
-        click_on 'Create Task'
-        expect(page).to have_content 'title test'
-      end
-    end
-  end
+  scenario "Test task list" do
 
-  describe 'list function' do
-    context 'to transition to the list screen' do
-      it 'already created tasks should be displayed' do
-        task = FactoryBot.create(:task, title: 'task')
-        visit task_path(task)
-        expect(page).to have_content 'task'
-      end
-    end
-  end
+   visit new_task_path
+   fill_in "task[title]", with: "test2"
+   fill_in "task[start_date]", with: Date.new(1/1/2020)
+   fill_in "task[end_date]", with: Date.new(1/2/2020)
+   select "in progress", from: "task[status]"
+   select "high", from: "task[priority]"
+   click_on 'search'
 
-  describe 'detailed display function' do
-    context 'to transition to any task detail screen' do
-      it 'contents of relevant task should be displayed' do
-        task = FactoryBot.create(:task, title: 'test2', content: 'test2')
-        visit task_path(task)
-        expect(page).to have_content 'test2'
-      end
-    end
-  end
+   visit new_task_path
+   fill_in "task[title]", with: "title3"
+   fill_in "task[start_date]", with: Date.new(1/1/2020)
+   fill_in "task[end_date]", with: Date.new(1/2/2020)
+   select "Not yet started", from: "task[status]"
+   select "low", from: "task[priority]"
+   click_on 'search'
 
-  let!(:task){ FactoryBot.create(:task, title: 'task') }
-  before do
-    FactoryBot.create(:task)
-    FactoryBot.create(:second_task)
-    FactoryBot.create(:third_task)
-    visit tasks_path
-  end
-  describe 'list display function' do
-    context 'When transitioning to the list screen' do
-      it 'already created tasks list should be displayed' do
-        expect(page).to have_content 'title test'
-      end
-    end
-    context 'When tasks are arranged in descending order of creation date and time' do
-      it 'New task is displayed at the top' do
-        assert Task.all.order(startdate: :desc)
-      end
-    end
-  end
+   visit tasks_path
+
+
+   expect(page).to have_content('title3')
+   expect(page).to have_content('low')
+   expect(page).to have_content('high')
+   expect(page).to have_content('in progress')
+   expect(page).to have_content('Not yet started')
+
+ end
+
+ scenario "Test task creation" do
+
+   visit new_task_path
+   fill_in "task[title]", with: "test2"
+   fill_in "task[start_date]", with: Date.new(1/1/2020)
+   fill_in "task[end_date]", with: Date.new(1/2/2020)
+   select "Not yet started", from: "task[status]"
+   select "high", from: "task[priority]"
+   click_on 'search'
+   expect(page).to have_content 'Task was successfully created'
+
+ end
+
+ scenario "Test of task details" do
+
+   visit new_task_path
+   fill_in "task[title]", with: "test2"
+   fill_in "task[start_date]", with: Date.new(1/1/2020)
+   fill_in "task[end_date]", with: Date.new(1/2/2020)
+   select "Not yet started", from: "task[status]"
+   select "high", from: "task[priority]"
+   click_on 'search'
+   visit tasks_path
+   click_on 'Show'
+   expect(page).to have_content "Not yet started"
+
+ end
+
+ scenario "Test whether tasks are arranged in descending order of creation date" do
+
+   visit new_task_path
+   fill_in "task[title]", with: "test2"
+   fill_in "task[start_date]", with: Date.new(1/1/2020)
+   fill_in "task[end_date]", with: Date.new(1/2/2020)
+   select "Not yet started", from: "task[status]"
+   select "high", from: "task[priority]"
+   click_on 'search'
+
+   visit new_task_path
+   fill_in "task[title]", with: "test2"
+   fill_in "task[start_date]", with: Date.new(1/1/2020)
+   fill_in "task[end_date]", with: Date.new(1/2/2020)
+   select "Not yet started", from: "task[status]"
+   select "low", from: "task[priority]"
+   click_on 'search'
+
+   visit tasks_path
+   tasks = page.all('tr')
+   expect(tasks[1]).to have_content('high')
+   expect(tasks[2]).to have_content('low')
+ end
+
+
+ scenario "Test search by status" do
+   visit new_task_path
+   fill_in "task[title]", with: "test2"
+   fill_in "task[start_date]", with: Date.new(1/1/2020)
+   fill_in "task[end_date]", with: Date.new(1/2/2020)
+   select "Not yet started", from: "task[status]"
+   select "low", from: "task[priority]"
+   click_on 'search'
+
+   visit tasks_path
+   fill_in "q[status_cont]", with: "Not yet started"
+   click_on 'search'
+   expect(page).to have_content "Not yet started"
+ end
+
+
+ scenario "Test search by title" do
+   visit new_task_path
+   fill_in "task[title]", with: "test2"
+   fill_in "task[start_date]", with: Date.new(1/1/2020)
+   fill_in "task[end_date]", with: Date.new(1/2/2020)
+   select "Not yet started", from: "task[status]"
+   select "low", from: "task[priority]"
+   click_on 'search'
+
+   visit tasks_path
+   fill_in "q[title_cont]", with: "test2"
+   click_on 'search'
+   expect(page).to have_content "test2"
+ end
+
+ scenario "Test sort by priority" do
+
+   visit new_task_path
+   fill_in "task[title]", with: "test2"
+   fill_in "task[start_date]", with: Date.new(1/1/2020)
+   fill_in "task[end_date]", with: Date.new(1/2/2020)
+   select "Not yet started", from: "task[status]"
+   select "high", from: "task[priority]"
+   click_on 'search'
+
+   visit new_task_path
+   fill_in "task[title]", with: "test2"
+   fill_in "task[start_date]", with: Date.new(1/1/2020)
+   fill_in "task[end_date]", with: Date.new(1/2/2020)
+   select "Not yet started", from: "task[status]"
+   select "low", from: "task[priority]"
+   click_on 'search'
+
+   visit tasks_path
+   click_on 'sort by priority'
+   tasks = page.all('tr')
+   expect(tasks[1]).to have_content('low')
+   expect(tasks[2]).to have_content('high')
+
+ end
+
 end
