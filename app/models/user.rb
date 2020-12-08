90af59eda6fa508b24fe1c20a75bb9837283968e
lompo <lompo@iuj.ac.jp>
2020-12-08 15:52:42 +0900

    fixing heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   validates :username , presence:true
   validates :email , presence:true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
-  #has_secure_password
+  has_secure_password
   validates :password, length: { minimum: 6 }
 end
